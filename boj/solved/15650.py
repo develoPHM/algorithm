13f501b51f32a5369ab6255427fbeb380f215246
phmok1@naver.com <phmok1@naver.com>
2024-08-12 15:03:09 +0900

    aja

---
+++ boj/solved/15650.py
@@ -0,0 +1,21 @@
+N, M = map(int, input().split())
+
+li = []
+
+
+def f(start):
+    if len(li) == M:
+        print(*map(str, li))
+        return
+
+    for i in range(start, N + 1):
+        if len(li) > 1 and i < li[-1]:
+            continue
+        else:
+            start += 1
+            li.append(i)
+            f(start - 1)
+            li.pop()
+
+
+f(1)
---
+++ boj/solved/15651.py
@@ -0,0 +1,19 @@
+N, M = map(int, input().split())
+
+li = []
+
+
+def f():
+    if len(li) == M:
+        print(*map(str, li))
+        return
+
+    for i in range(1, N + 1):
+        # if i in li:
+        #     continue
+        li.append(i)
+        f()
+        li.pop()
+
+
+f()
