7630ba6d4fed78078f5e33df902d54fc60627ea8
phmok1@naver.com <phmok1@naver.com>
2024-08-13 17:53:31 +0900

    aja

--- boj/solved/15650.py
+++ boj/solved/15650.py
@@ -1,7 +1,28 @@
+# N, M = map(int, input().split())
+#
+# li = []
+#
+#
+# def f(start):
+#     if len(li) == M:
+#         print(*map(str, li))
+#         return
+#
+#     for i in range(start, N + 1):
+#         if len(li) > 1 and i < li[-1]:
+#             continue
+#         else:
+#             start += 1
+#             li.append(i)
+#             f(start - 1)
+#             li.pop()
+#
+#
+# f(1)
+
+
 N, M = map(int, input().split())
-
 li = []
-
 
 def f(start):
     if len(li) == M:
@@ -9,13 +30,9 @@ def f(start):
         return
 
     for i in range(start, N + 1):
-        if len(li) > 1 and i < li[-1]:
-            continue
-        else:
-            start += 1
-            li.append(i)
-            f(start - 1)
-            li.pop()
-
+        li.append(i)
+        f(i + 1)
+        li.pop()
+        # start += 1
 
 f(1)
---
+++ boj/solved/9663.py
@@ -0,0 +1,11 @@
+N = int(input())
+graph = [[False] * N for _ in range(N)]
+
+def queen():
+    for i in range(N):
+        for j in range(N):
+            graph[][]
+
+
+for c in graph:
+    print(c)
