88d67f2a86fe851ead10be7cc56aaf86efa95c61
phmok1@naver.com <phmok1@naver.com>
2024-08-24 21:00:06 +0900

    aja

--- boj/re_solved/15649.py
+++ boj/re_solved/15649.py
@@ -1,20 +1,22 @@
-def dfs(n, lst):
+# n = 선택된 숫자 개수 (길이)
+def dfs(n, li):
     if n == M:  # M개의 수열을 완성
-        ans.append(lst)
+        ans.append(li)
         return
     # 하부 단계 호출
-    for j in range(1, N + 1):
-        if v[j] == 0:
-            v[j] = 1
-            dfs(n + 1, lst + [j])
-            v[j] = 0
+    for i in range(1, N + 1):
+        if not v[i]:
+            v[i] = True
+            dfs(n + 1, li + [i])
+            v[i] = False
 
 
 N, M = map(int, input().split())
 
 ans = []  # 정답 리스트를 저장할 리스트
-v = [0] * (N + 1)  # 중복확인
+v = [False] * (N + 1)  # 중복확인
 
-dfs(0, [])
+dfs(0, []) # 시작
 for c in ans:
     print(*c)
+b
---
+++ boj/re_solved/15652.py
@@ -0,0 +1,13 @@
+N, M = map(int,input().split())
+li = []
+def f(start):
+    if len(li) == M:
+        print(*li)
+        return
+    for i in range(start, N + 1):
+        li.append(i)
+        f(i)
+        li.pop()
+
+
+f(1)
