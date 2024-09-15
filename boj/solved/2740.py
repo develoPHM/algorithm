61336354251f892203a5fa800783c60c1aa9a766
phmok1@naver.com <phmok1@naver.com>
2024-09-15 18:49:02 +0900

    aja

--- boj/solved/2740.py
+++ boj/solved/2740.py
@@ -1,4 +1,3 @@
-li = []
 li_1 = []
 li_2 = []
 N, M = map(int,input().split())
@@ -7,6 +6,13 @@ for i in range(N):
 
 M, K = map(int,input().split())
 for i in range(M):
-    li.append(list(map(int,input().split())))
-for i in range(len(li)):
-    li_2.append([li[i]])
+    li_2.append(list(map(int,input().split())))
+
+li = [[0] * K for _ in range(N)]
+
+for a in range(N):
+    for b in range(K):
+        for c in range(M):
+            li[a][b] += li_1[a][c] * li_2[c][b]
+for c in li:
+    print(*c)
