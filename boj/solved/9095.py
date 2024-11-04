efe3c73c4a1cfa3a9e3df175911ee51e042e2260
phmok1@naver.com <phmok1@naver.com>
2024-11-04 16:59:08 +0900

    study

--- boj/solved/9095.py
+++ boj/solved/9095.py
@@ -1,4 +1,16 @@
+dp = [0] * 11
+dp[0] = 1
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+dp[4] = 7
+dp[5] = 13
+dp[6] = 24
+dp[7] = 44
+dp[8] = 81
+dp[9] = 149
+dp[10] = 274
 T = int(input())
 for _ in range(T):
     num = int(input())
-    c
+    print(dp[num])
