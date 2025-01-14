bc9aeceb7fc8c0e0b8d70270134aa377a35a128b
phmok1@naver.com <phmok1@naver.com>
2025-01-14 16:42:24 +0900

    study

--- programmers/Lv_2/귤 고르기.py
+++ programmers/Lv_2/귤 고르기.py
@@ -1,21 +1,15 @@
-k = 2
-tangerine = [1, 1, 1, 1, 2, 2, 2, 3, 4]
-dic = {}
-plus= 0
-cnt = 0
-for c in tangerine:
-    if c in dic:
-        dic[c] += 1
-    else:
-        dic[c] = 1
-sorted_dic = dict(sorted(dic.items(),reverse=True, key=lambda item: item[1]))
-print(sorted_dic)
-for key, value in sorted_dic.items():
-    if plus == k:
-        print(cnt)
-        break
-    if plus + value > k:
-        continue
-    else:
-        plus += value
-        cnt += 1
+def solution(k, tangerine):
+    dic = {}
+    ans = 0
+    for c in tangerine:
+        if c in dic:
+            dic[c] += 1
+        else:
+            dic[c] = 1
+    dic = dict(sorted(dic.items(), reverse=True, key=lambda x: x[1]))
+    for c in dic:
+        if k <= 0:
+            return ans
+        k -= dic[c]
+        ans += 1
+    return ans
