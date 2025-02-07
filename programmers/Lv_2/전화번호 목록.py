9f8bb65559037ae67fd39c373041b9900c7b4e00
phmok1@naver.com <phmok1@naver.com>
2025-02-07 17:52:54 +0900

    study

--- programmers/Lv_2/전화번호 목록.py
+++ programmers/Lv_2/전화번호 목록.py
@@ -1,1 +1,21 @@
-print(int('2'))
+# def solution(phone_book):
+#     N = len(phone_book)
+#     li = sorted(phone_book)
+#     for i in range(N - 1):
+#         length = len(li[i])
+#         if li[i] in li[i + 1][0:length]:
+#             return False
+#     return True
+
+def solution(phone_book):
+    answer = True
+    dic = {}
+    for c in phone_book:
+        dic[c] = 1
+    for c in phone_book:
+        s = ""
+        for number in c:
+            s += number
+            if s in dic and s != c:
+                answer = False
+    return answer
