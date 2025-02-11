dccec2f91ca8f351f99c11de28699a0091921ba6
phmok1@naver.com <phmok1@naver.com>
2025-02-11 17:12:39 +0900

    study

---
+++ new/boj/27866.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int num;
+  string s;
+
+  cin >> s;
+  cin >> num;
+
+  cout << s[num - 1];
+}
---
+++ new/boj/9506.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    while(true) {
+        int a;
+        cin >> a;
+
+        if (a == -1) {
+            break;
+        }
+        for (int i = 1; i < a/)
+    }
+}
