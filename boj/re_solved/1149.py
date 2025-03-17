7c803414f0221408828c27b5dea2c891363f2463
develoPHM <phmok1@naver.com>
2025-03-17 23:27:52 +0900

    study

--- boj/re_solved/1149.py
+++ boj/re_solved/1149.py
@@ -8,4 +8,5 @@ for i in range(1, N):
     dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + price[i][0]
     dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + price[i][1]
     dp[i][2] = min(dp[i - 1][1], dp[i - 1][0]) + price[i][2]
+# print(price)
 print(min(dp[-1]))
