900771187846520517bec03ef8be0f43cf66d08e
develoPHM <phmok1@naver.com>
2025-11-27 17:22:37 +0900

    study

--- programmers/Lv_2/짝지어 제거하기.py
+++ programmers/Lv_2/짝지어 제거하기.py
@@ -1,10 +1,15 @@
-s = 'cdcd'
-arr = []
-for c in s:
+def solution(s):
+    arr = [s[0]]
+    for i in range(1, len(s)):
+        if len(arr) == 0:
+            arr.append(s[i])
+            continue
+        if arr[-1] == s[i]:
+            arr.pop()
+        else:
+            arr.append(s[i])
     if len(arr) == 0:
-        arr.append(c)
-        continue
-    if arr[-1] == c:
-        arr.pop()
-        continue
-    arr.append(c)
+        return 1
+    else:
+        return 0
+
--- programmers/Lv_2/피보나치 수.py
+++ programmers/Lv_2/피보나치 수.py
@@ -1,7 +1,6 @@
 def solution(n):
-    ans = [0, 1]
-
+    dp = [0] * (n + 1)
+    dp[0], dp[1] = 0, 1
     for i in range(2, n + 1):
-        ans.append((ans[i - 1] + ans[i - 2]) % 1234567)
-
-    return ans[-1]
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567
+    return dp[n]
