e6d5f802b1434880c4594f439c3abb8af054314d
develoPHM <phmok1@naver.com>
2025-12-02 18:23:15 +0900

    study

--- programmers/Lv_2/카펫.py
+++ programmers/Lv_2/카펫.py
@@ -1,16 +1,6 @@
 def solution(brown, yellow):
-    ans = []
-
-    yellow_x = 0
-    yellow_y = 0
-
-    for i in range(1, yellow + 1):
-        if yellow % i == 0:
-            yellow_x = int(yellow / i)
-            yellow_y = i
-            if yellow_x * 2 + yellow_y * 2 + 4 == brown:
-                ans.append(yellow_x + 2)
-                ans.append(yellow_y + 2)
-                break
-    ans.sort(reverse=True)
-    return ans
+    answer = []
+    n = int(brown/2 + 2)
+    for i in range(n-3, int(n/2 - 1), -1):
+        if (i-2)*(n-i-2) == yellow:
+            return [i, n-i]
