9b1eab069bd21a4ef3ae8de55fd584f803f90f7a
develoPHM <phmok1@naver.com>
2025-12-05 17:29:51 +0900

    study

--- README.md
+++ README.md
@@ -6,4 +6,10 @@
   목표는 골드 1, Lv_3
 </h1>
 <br>
-연속된 자연수의 합 -  투 포인터
+
+투포인터 - 연속된 자연수의 합
+| 알고리즘 | 유형 | 링크 |   |   |
+|---|---|---|---|---|
+| 투포인터 | 연속된 자연수의 합  |  https://school.programmers.co.kr/learn/courses/30/lessons/12924 |   |   |
+|   | 구명보트 | https://school.programmers.co.kr/learn/courses/30/lessons/42885 |   |   |
+|   |   |   |   |   |
--- programmers/Lv_2/구명보트.py
+++ programmers/Lv_2/구명보트.py
@@ -2,11 +2,11 @@ def solution(people, limit) :
     cnt = 0
     people.sort()
 
-    l = 0
-    r = len(people) - 1
-    while l < r :
-        if people[r] + people[l] <= limit :
-            l += 1
+    a = 0
+    b = len(people) - 1
+    while a < b :
+        if people[b] + people[a] <= limit :
+            a += 1
             cnt += 1
-        r -= 1
-    return len(people) - cnt
+        b -= 1
+    return len(people) - answer
