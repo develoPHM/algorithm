1aa0f427387a3a94185dea89970fbc0b66da03b2
develoPHM <phmok1@naver.com>
2026-01-07 17:03:18 +0900

    study

--- programmers/Lv_2/점프와 순간 이동.py
+++ programmers/Lv_2/점프와 순간 이동.py
@@ -1,9 +1,10 @@
-N = 5
-cnt = 0
-
-while N > 0:
-    if N % 2 == 1:
-        N -= 1
-        cnt += 1
-    else:
-        N = N // 2
+# k칸 점프 건전지 - k, 온 거리 x2 순간이동
+# 반대로 6번째 칸에서 0으로 간다고 생각하면 쉬움
+n = 6
+ans = 0
+while n > 0:
+    if n % 2 != 0:
+       ans += 1
+       n -= 1
+    n = n // 2
+print(ans)
