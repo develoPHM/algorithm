9520c8280f7d78dcf2aa55fd51b9f467e2a0508d
develoPHM <phmok1@naver.com>
2026-01-08 23:23:21 +0900

    study

--- programmers/Lv_2/연속 부분 수열 합의 개수.py
+++ programmers/Lv_2/연속 부분 수열 합의 개수.py
@@ -13,3 +13,13 @@ def solution(elements):
         arr.update(dp)
 
     return len(arr)
+
+# --------------------------------------- #
+def solution(el):
+    el_span = el * 2
+    li = set()
+    for length in range(1, len(el) + 1): # 수열 길이
+        for start in range(0, len(el)): # 시작점
+            sumNum = sum(el_span[start: start + length])
+            li.add(sumNum)
+    return len(li)
