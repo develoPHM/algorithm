4ba17ccb702224d6fdee756f4087b351f2699c26
develoPHM <phmok1@naver.com>
2026-01-08 13:13:50 +0900

    study

--- programmers/Lv_2/N개의 최소공배수.py
+++ programmers/Lv_2/N개의 최소공배수.py
@@ -6,3 +6,22 @@ def solution(arr):
         ans = ans * c // gcd(ans, c)
 
     return ans
+
+#-----------------------------------------#
+def solution(arr):
+    # 최대공약수(GCD) (유클리드 호제법)
+    def get_gcd(a, b):
+        while b > 0:
+            a, b = b, a % b
+        return a
+
+    # 최소공배수(LCM)
+    def get_lcm(a, b):
+        return (a * b) // get_gcd(a, b)
+
+    # 배열의 첫 번째 요소부터 시작해서 연쇄적으로 LCM을 구함
+    answer = arr[0]
+    for i in range(1, len(arr)):
+        answer = get_lcm(answer, arr[i])
+
+    return answer
