f895ba535b3a2e920a4e24348c209595de48faab
develoPHM <phmok1@naver.com>
2026-01-08 13:47:43 +0900

    study

--- programmers/Lv_2/멀리 뛰기.py
+++ programmers/Lv_2/멀리 뛰기.py
@@ -6,3 +6,14 @@ def solution(n):
         dp[i] = dp[i - 2] + dp[i - 1]
 
     return dp[n - 1] % 1234567
+#--------------#
+def solution(n):
+    if n < 3:
+        return n
+    arr = [0] * 2001
+    arr[0] = 0
+    arr[1] = 1
+    arr[2] = 2
+    for i in range(3, len(arr)):
+        arr[i] = (arr[i - 2] + arr[i - 1]) % 1234567
+    return arr[n]
